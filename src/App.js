267fef7b153581d08bd3495d34cffc147a09e2ed
Archana Maurya <mauryaarchana083@gmail.com>
2024-09-12 07:09:22 +0530

    created app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="container">
+      <h1 className="text-center">Hello, Bootstrap in React!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
